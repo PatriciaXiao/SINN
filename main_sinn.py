690dc0a8c6ba5f37d04126ce0ea0d73edec41f76
patricia_xiao <PatriciaXiao@users.noreply.github.com>
2023-09-25 19:02:14 -0700

    Update main_sinn.py

--- main_sinn.py
+++ main_sinn.py
@@ -58,8 +58,9 @@ def prediction2label(x):
 def rolling_matrix(x,window_size=21):
     # https://numpy.org/doc/stable/reference/generated/numpy.lib.stride_tricks.as_strided.html
     x = x.flatten()
-    n = x.shape[0]
-    stride = x.strides[0] # number of users+1
+    n = x.shape[0] # number of users+1
+    # https://numpy.org/doc/stable/reference/generated/numpy.ndarray.strides.html
+    stride = x.strides[0] # Tuple of bytes to step in each dimension when traversing an array.
     return np.lib.stride_tricks.as_strided(x, shape=(n-window_size+1, window_size), strides=(stride,stride) ).copy()
 
 
